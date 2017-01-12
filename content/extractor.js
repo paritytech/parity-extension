25d7a6924e1446eb18bb8da3d55a45e01428e23d
Tomasz Drwięga <tomasz@ethcore.io>
2017-01-12 14:41:31 +0100

    Processing DOM nodes

---
+++ content/extractor.js
@@ -0,0 +1,4 @@
+// Given DOM element returns array of possible id-links to resolve.
+export function extractPossibleMatches ($dom) {
+  return [];
+}
--- content/index.js
+++ content/index.js
@@ -1,14 +1,44 @@
-const body = document.body
+import { extractPossibleMatches } from './extractor'
 
 const port = chrome.runtime.connect({ name: 'id' })
 
-// Parse initial page
-const treeWalker = document.createTreeWalker(body, NodeFilter.SHOW_TEXT);
-while (treeWalker.nextNode()) {
-  console.log(`Visiting: `, treeWalker.currentNode)
-  port.postMessage(treeWalker.currentNode.textContent)
+// Process the page in stages.
+// 0. We listen for possible changes
+// 1. First we look for most likely matches <a href="mailto:..> and <a href="{user_profile}">
+// 2. Then we process all text nodes
+
+function stage1(done) {
+  const treeWalker = document.createTreeWalker(document.body, NodeFilter.SHOW_ELEMENT)
+  while (treeWalker.nextNode()) {
+    console.log('Visiting: ', treeWalker.currentNode)
+
+    const matches = extractPossibleMatches(treeWalker.currentNode)
+    if (matches.length) {
+      port.postMessage({
+        type: 'processLinks',
+        data: matches
+      })
+    }
+  }
+
+  done()
 }
 
+function stage2(done) {
+  const treeWalker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT)
+  while (treeWalker.nextNode()) {
+    console.log(`Text: `, treeWalker.currentNode)
+
+    port.postMessage({
+      type: 'processText',
+      data: [treeWalker.currentNode.textContent]
+    })
+  }
+
+  done()
+}
+
+// Listen for responses
 port.onMessage.addListener(msg => {
   console.log('Got response: ', msg)
 })
@@ -18,10 +48,17 @@ const observer = new MutationObserver(mutations => {
   mutations.forEach(mutation => console.log(mutation))
 })
 
-observer.observe(body, {
+observer.observe(document.body, {
   attributes: true,
   childList: true,
   characterData: true
+})
+
+// Start processing
+stage1(() => {
+  setTimeout(() => {
+    stage2(() => {})
+  }, 500)
 })
 
 

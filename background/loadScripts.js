d6b5bff22f765e9ef676e82b400f83059b0bf7e1
Nicolas Gotchac <ngotchac@gmail.com>
2017-01-26 16:10:56 +0100

    Remove wrong file

--- background/loadScripts.js
+++ background/loadScripts.js
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-import { uniq } from 'lodash';
+// import { uniq } from 'lodash';
 
 import { UI, EV_BAR_CODE, getRetryTimeout } from '../shared';
 

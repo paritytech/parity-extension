343007dd0d75d401bf0656b23cb5f7d7a13fa11e
Nicolas Gotchac <ngotchac@gmail.com>
2017-02-08 13:34:21 +0100

    Added connection status to the Pop-Up

--- popup/component/app.js
+++ popup/component/app.js
@@ -14,11 +14,13 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
+import classnames from 'classnames';
 import { h, Component } from 'preact';
 
 import 'material-design-lite/material.css';
 import 'material-design-lite/material';
 
+import Config from '../../background/config';
 import Extractions from './extractions';
 
 import styles from './app.css';
@@ -26,10 +28,18 @@ import styles from './app.css';
 export default class App extends Component {
 
   state = {
+    augmentationEnabled: true,
     extractions: []
   };
 
   componentWillMount () {
+    Config.get()
+      .then((config) => {
+        const { augmentationEnabled } = config;
+
+        this.setState({ augmentationEnabled });
+      });
+
     this.getExtractions();
 
     // Trigger when the pop-up is open
@@ -45,7 +55,10 @@ export default class App extends Component {
   }
 
   render () {
-    const { extractions } = this.state;
+    const { augmentationEnabled, extractions } = this.state;
+
+    const status = 'disconnected';
+    const url = 'http://localhost:8545/';
 
     return (
       <div className={ styles.container }>
@@ -53,13 +66,54 @@ export default class App extends Component {
           <h1 className={ styles.title }>Web3 Injection</h1>
         </div>
 
-        <Extractions
-          extractions={ extractions }
-        />
+        { this.renderExtractions(augmentationEnabled, extractions) }
 
-        <div className={ styles.status }>
-          Connected to http://localhost:8545/
-        </div>
+        { this.renderStatus(status, url) }
+      </div>
+    );
+  }
+
+  renderExtractions (augmentationEnabled, extractions) {
+    if (!augmentationEnabled) {
+      return null;
+    }
+
+    return (
+      <Extractions
+        extractions={ extractions }
+      />
+    );
+  }
+
+  renderStatus (status, url) {
+    const iconClassName = classnames({
+      [ styles.statusIcon ]: true,
+      [ styles.connected ]: status === 'connected',
+      [ styles.connecting ]: status === 'connecting',
+      [ styles.disconnected ]: status === 'disconnected'
+    });
+
+    let phrase;
+
+    switch (status) {
+      case 'connected':
+        phrase = 'Connected to';
+        break;
+
+      case 'connecting':
+        phrase = 'Connecting to';
+        break;
+
+      case 'disconnected':
+      default:
+        phrase = 'Not connected to';
+        break;
+    }
+
+    return (
+      <div className={ styles.status }>
+        <span className={ iconClassName } />
+        <span>{ phrase } { url }</span>
       </div>
     );
   }

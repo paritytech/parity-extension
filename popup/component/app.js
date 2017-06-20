9004588b89ca8dfad5cc5f7053024ec776ea1342
Daniel Robinson <danrobinson010@gmail.com>
2017-06-20 04:01:12 -0400

    Fix typo in popup.

--- popup/component/app.js
+++ popup/component/app.js
@@ -30,7 +30,7 @@ export default class App extends Component {
 
   state = {
     augmentationEnabled: true,
-    chainName: 'an unkown chain',
+    chainName: 'an unknown chain',
     extractions: [],
     status: ''
   };
